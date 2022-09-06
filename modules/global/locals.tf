8b6f824ebf275e41b5969b723544c707ba1047ef
Brendan Thompson <github@blt.is>
2022-09-06 15:19:32 +1000

    chore: use locals file for global locals

---
+++ modules/global/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  github = {
+    organization = "ministry-of-magic"
+  }
+  terraform = {
+    organization   = "ministry-of-magic"
+    oauth_token_id = "ot-5q4YcnqqChBvJEcg"
+  }
+}
--- modules/global/outputs.tf
+++ modules/global/outputs.tf
@@ -1,13 +1,3 @@
-locals {
-  github = {
-    organization = "ministry-of-magic"
-  }
-  terraform = {
-    organization   = "ministry-of-magic"
-    oauth_token_id = "ot-5q4YcnqqChBvJEcg"
-  }
-}
-
 output "github" {
   value = local.github
 }
